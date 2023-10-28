81c4ca8f31e5875877e88186bcb7de8be9864b9f
Le Vivilet <levivlet@pm.me>
2023-10-28 21:16:22 +0200

    feature: add eslint plugin vue tests

---
+++ test/cases/eslint-plugin-vue-script-indent-ts-abstract-class-property-02.vue
@@ -0,0 +1,19 @@
+<!--{"parserOptions": {"parser":"@typescript-eslint/parser"}}-->
+<script lang="ts">
+abstract class A {
+  a
+    =
+    1
+  ;
+  abstract public b
+  // parser v5 does not parse value.
+  //   =
+  //   's'
+  ;
+  protected abstract c
+  // parser v5 does not parse value.
+  //   =
+  //   i
+  ;
+}
+</script>
---
+++ test/cases/eslint-plugin-vue-script-indent-ts-class-declaration-05.vue
@@ -0,0 +1,14 @@
+<!--{"parserOptions": {"parser":"@typescript-eslint/parser"}}-->
+<script lang="ts">
+class
+  Foo
+  extends
+    Bar
+  implements
+    Baz1
+    ,
+    Baz2
+{
+  prop:string
+}
+</script>
